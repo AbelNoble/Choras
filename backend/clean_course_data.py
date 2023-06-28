7aa813b0285455ae47be8eedc0cbc7bff7bd5f75
Abel Noble <abel.noble3@gmail.com>
2023-06-28 12:28:16 -0400

    finished cleaning uofm fall 2023

--- backend/clean_course_data.py
+++ backend/clean_course_data.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 # Read the CSV file
-data = pd.read_csv('class_information.csv')
+data = pd.read_csv('../database/university_of_michigan/fall2023.csv')
+data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
 
 # Remove irrelevant columns
-data = data.drop(['code'], axis=1)
+data = data.drop(['Term', 'Session', 'Codes', 'S', 'SU'], axis=1)
+print('\nRemoved Irrelevant Columns')
 
-# Map abbreviated days to full names
+# Create new days column
 days_mapping = {
     'M': 'Monday',
     'T': 'Tuesday',
@@ -14,23 +16,41 @@ days_mapping = {
     'TH': 'Thursday',
     'F': 'Friday'
 }
-
-# Create 'days_of_week' column
 day_columns = ['M', 'T', 'W', 'TH', 'F']
-data['days_of_week'] = data[day_columns].apply(
+data.insert(12, 'Days', data[day_columns].apply(
     lambda x: ', '.join(days_mapping[day] for day in x.dropna()), axis=1
-)
-
-# Drop the individual day columns
+))
 data = data.drop(day_columns, axis=1)
+print('\nInserted New Days Column')
 
-# Extract catalog number
-data['catalog_number'] = data['class_number'].apply(lambda x: x.split('-')[0])
+# Expanded components and removed clinical experience classes
+component_mapping = {
+    'LAB': 'Lab',
+    'LEC': 'Lecture',
+    'SEM': 'Seminar',
+    'DIS': 'Discussion',
+    'REC': 'Recitation',
+    'IND': 'Individual Instruction'
+}
+data['Component'] = data['Component'].map(component_mapping)
+data = data[data['Component'] != 'CLN']
+data = data[data['Component'].notna()]
+print('\nInserted New Component Column')
+
+# Move section to 2nd row
+cols = list(data.columns)
+cols.remove('Section')
+cols.insert(0, 'Section')
+data = data.reindex(columns=cols)
+print('\nMoved Section Column')
 
 # Move catalog number to the beginning
 cols = list(data.columns)
-cols = ['catalog_number'] + [col for col in cols if col != 'catalog_number']
+cols = ['Catalog Nbr'] + [col for col in cols if col != 'Catalog Nbr']
 data = data[cols]
+print('\nMoved Catalog Column')
 
 # Write cleaned data to a new CSV file
-data.to_csv('cleaned_class_information.csv', index=False)
+output_path = '../database/university_of_michigan/cleaned_fall2023.csv'
+data.to_csv(output_path, index=False)
+print('\nSaved File')
