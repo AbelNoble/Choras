ca6d650127045db4f91fe235fcbabb6f426e49ab
Abel Noble <abel.noble3@gmail.com>
2023-07-14 14:14:59 -0400

    finished tabular-to-text

--- backend/contextualize_course_data.py
+++ backend/contextualize_course_data.py
@@ -9,9 +9,8 @@ with open(output_path, 'w') as file:
     # Iterate over each row in the DataFrame
     for _, row in data.iterrows():
         # Generate the prompt sentence for the current row
-        prompt = f'This class, catalog number {row["Catalog Nbr"]}, section {row["Section"]}, and class number {row["Class Nbr"]}, ' \
-                 f'belongs to the {row["Acad Group"]} academic group. It is a {row["Component"]} component of the course titled "{row["Course Title"]}" ' \
-                 f'about the subject of {row["Subject"]}. The class is scheduled on the day(s) {row["Days"]} at the time {row["Time"]} and will be held in room {row["Location"]}. ' \
+        prompt = f'This class is a {row["Component"]} component of the course{row["Catalog Nbr"]} with a class number {row["Class Nbr"]}. It is section {row["Section"]} of the class titled "{row["Course Title"]}" ' \
+                 f'belonging to the {row["Acad Group"]} academic group about the subject of {row["Subject"]}. The class is scheduled on the day(s) {row["Days"]} at the time {row["Time"]} and will be held in room {row["Location"]}. ' \
                  f'The instructor for this class is {row["Instructor"]}. It is a {row["Units"]} unit course and will run for the duration of the {row["Class Duration"]}.\n'
 
         # Write the prompt to the Python file
