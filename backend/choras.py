5124d1cc408e1863c9afdc4c19a681433b09aea9
Abel Noble <abel.noble3@gmail.com>
2023-07-19 03:18:23 -0400

    waaas lowk sscared it wouldnt work

--- backend/choras.py
+++ backend/choras.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 from langchain.llms import OpenAI
 # text stuff
 from langchain.document_loaders import TextLoader
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import NLTKTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
 # vector store stuff
 from langchain.vectorstores import Chroma
 from langchain.agents.agent_toolkits import (create_vectorstore_agent, VectorStoreToolkit, VectorStoreInfo)
@@ -20,7 +20,8 @@ load_dotenv()
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 
 # sets llm **creativity**
-llm = OpenAI(temperature=0.9)
+llm = OpenAI(temperature=1, verbose=True)
+embeddings = OpenAIEmbeddings()
 
 # loads cleaned text file
 file_path = '/Users/abelnoble/Desktop/Choras/database/university_of_michigan/text_fall2023.txt'
@@ -32,18 +33,14 @@ with open(file_path, "w", encoding="utf-8") as f:
 loader = TextLoader(file_path)
 documents = loader.load()
 
-# chunkz
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+nltk_text_splitter = NLTKTextSplitter(chunk_size=1000)
 
-# random open source embedding transformer (may need to change)
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+docs = nltk_text_splitter.create_documents([text])
 
-# stored into chroma
-db = Chroma.from_documents(docs, embedding_function)
+vectordb = Chroma.from_documents(documents=docs, embedding=embeddings)
 
 # calling an agent to help out
-vectorstore_info = VectorStoreInfo(name='ChorasDB', description='Course Data', vectorstore=db)
+vectorstore_info = VectorStoreInfo(name='ChorasDB', description='Course Data', vectorstore=vectordb)
 toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
 agent = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=True)
 
@@ -55,12 +52,13 @@ st.title('🌸 Choras: Your AI Academic Advisor')
 st.divider()
 
 prompt = st.text_input('Let me help you find a class:')
-st.caption('Ask \"What\'s a 4 credit class about Architecture that I can take in the mornings?')
+st.caption('Ask \"What\'s a 4 credit class about Architecture that I can take in the mornings?\"')
 
 if prompt:
+    print("Prompt length:", len(prompt))
     response = agent.run(prompt)
     st.write(response)
 
-    with st.expander('Similarity Search'):
-        search = db.similarity_search_with_score(prompt)
-        st.write(search[0][0].page_content)
+    # with st.expander('Similarity Search'):
+    #     search = vectordb.similarity_search_with_score(prompt)
+    #     st.write(search[0][0].page_content)
